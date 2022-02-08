69973d5e6c132b3573aeb879b92a888e705fca65
Luka Simovic <92988092+gregorije@users.noreply.github.com>
2022-02-08 12:56:32 +0100

    Final push with application setup

---
+++ TpsFileEditor/TpsFileEditor-main/TpsFileEditor/TpsFileEditor/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace TpsFileEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bunifuCustomLabel4 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.bunifuThinButton21 = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.ddlRoot = new Bunifu.Framework.UI.BunifuDropdown();
+            this.bunifuCustomLabel3 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.tbBrVrata = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.tbPakId = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.bunifuCustomLabel4);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 76);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TpsFileEditor.Properties.Resources.quit;
+            this.pictureBox1.Location = new System.Drawing.Point(691, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // bunifuCustomLabel4
+            // 
+            this.bunifuCustomLabel4.AutoSize = true;
+            this.bunifuCustomLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.bunifuCustomLabel4.Location = new System.Drawing.Point(257, 19);
+            this.bunifuCustomLabel4.Name = "bunifuCustomLabel4";
+            this.bunifuCustomLabel4.Size = new System.Drawing.Size(258, 33);
+            this.bunifuCustomLabel4.TabIndex = 3;
+            this.bunifuCustomLabel4.Text = "TPS FILE EDITOR";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.panel2.Location = new System.Drawing.Point(0, 74);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 75);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.bunifuThinButton21);
+            this.panel3.Controls.Add(this.ddlRoot);
+            this.panel3.Controls.Add(this.bunifuCustomLabel3);
+            this.panel3.Controls.Add(this.bunifuCustomLabel2);
+            this.panel3.Controls.Add(this.bunifuCustomLabel1);
+            this.panel3.Controls.Add(this.tbBrVrata);
+            this.panel3.Controls.Add(this.tbPakId);
+            this.panel3.Location = new System.Drawing.Point(0, 147);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 344);
+            this.panel3.TabIndex = 2;
+            // 
+            // bunifuThinButton21
+            // 
+            this.bunifuThinButton21.ActiveBorderThickness = 1;
+            this.bunifuThinButton21.ActiveCornerRadius = 20;
+            this.bunifuThinButton21.ActiveFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.bunifuThinButton21.ActiveForecolor = System.Drawing.Color.White;
+            this.bunifuThinButton21.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.bunifuThinButton21.BackColor = System.Drawing.Color.White;
+            this.bunifuThinButton21.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuThinButton21.BackgroundImage")));
+            this.bunifuThinButton21.ButtonText = "Generisi";
+            this.bunifuThinButton21.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuThinButton21.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuThinButton21.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.bunifuThinButton21.IdleBorderThickness = 1;
+            this.bunifuThinButton21.IdleCornerRadius = 20;
+            this.bunifuThinButton21.IdleFillColor = System.Drawing.Color.White;
+            this.bunifuThinButton21.IdleForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.bunifuThinButton21.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.bunifuThinButton21.Location = new System.Drawing.Point(264, 275);
+            this.bunifuThinButton21.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.bunifuThinButton21.Name = "bunifuThinButton21";
+            this.bunifuThinButton21.Size = new System.Drawing.Size(264, 39);
+            this.bunifuThinButton21.TabIndex = 6;
+            this.bunifuThinButton21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuThinButton21.Click += new System.EventHandler(this.bunifuThinButton21_Click);
+            // 
+            // ddlRoot
+            // 
+            this.ddlRoot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.ddlRoot.BorderRadius = 3;
+            this.ddlRoot.DisabledColor = System.Drawing.Color.White;
+            this.ddlRoot.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ddlRoot.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.ddlRoot.Items = new string[0];
+            this.ddlRoot.Location = new System.Drawing.Point(397, 196);
+            this.ddlRoot.Margin = new System.Windows.Forms.Padding(4);
+            this.ddlRoot.Name = "ddlRoot";
+            this.ddlRoot.NomalColor = System.Drawing.Color.White;
+            this.ddlRoot.onHoverColor = System.Drawing.Color.White;
+            this.ddlRoot.selectedIndex = -1;
+            this.ddlRoot.Size = new System.Drawing.Size(312, 37);
+            this.ddlRoot.TabIndex = 5;
+            // 
+            // bunifuCustomLabel3
+            // 
+            this.bunifuCustomLabel3.AutoSize = true;
+            this.bunifuCustomLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.bunifuCustomLabel3.Location = new System.Drawing.Point(90, 196);
+            this.bunifuCustomLabel3.Name = "bunifuCustomLabel3";
+            this.bunifuCustomLabel3.Size = new System.Drawing.Size(130, 29);
+            this.bunifuCustomLabel3.TabIndex = 4;
+            this.bunifuCustomLabel3.Text = "Server ruta";
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoSize = true;
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(90, 124);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(114, 29);
+            this.bunifuCustomLabel2.TabIndex = 3;
+            this.bunifuCustomLabel2.Text = "Broj vrata";
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(90, 45);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(156, 29);
+            this.bunifuCustomLabel1.TabIndex = 2;
+            this.bunifuCustomLabel1.Text = "Paketomat ID";
+            // 
+            // tbBrVrata
+            // 
+            this.tbBrVrata.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.tbBrVrata.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.tbBrVrata.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.tbBrVrata.BorderThickness = 1;
+            this.tbBrVrata.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbBrVrata.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbBrVrata.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.tbBrVrata.isPassword = false;
+            this.tbBrVrata.Location = new System.Drawing.Point(397, 114);
+            this.tbBrVrata.Margin = new System.Windows.Forms.Padding(4);
+            this.tbBrVrata.Name = "tbBrVrata";
+            this.tbBrVrata.Size = new System.Drawing.Size(312, 44);
+            this.tbBrVrata.TabIndex = 1;
+            this.tbBrVrata.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // tbPakId
+            // 
+            this.tbPakId.BorderColorFocused = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.tbPakId.BorderColorIdle = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.tbPakId.BorderColorMouseHover = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.tbPakId.BorderThickness = 1;
+            this.tbPakId.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbPakId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPakId.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(79)))), ((int)(((byte)(109)))));
+            this.tbPakId.isPassword = false;
+            this.tbPakId.Location = new System.Drawing.Point(397, 36);
+            this.tbPakId.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPakId.Name = "tbPakId";
+            this.tbPakId.Size = new System.Drawing.Size(312, 44);
+            this.tbPakId.TabIndex = 0;
+            this.tbPakId.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 492);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ImageList imageList1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Panel panel3;
+        private Bunifu.Framework.UI.BunifuMetroTextbox tbPakId;
+        private System.Windows.Forms.Panel panel2;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuMetroTextbox tbBrVrata;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel3;
+        private Bunifu.Framework.UI.BunifuDropdown ddlRoot;
+        private Bunifu.Framework.UI.BunifuThinButton2 bunifuThinButton21;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TpsFileEditor/TpsFileEditor-main/TpsFileEditor/TpsFileEditor/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace TpsFileEditor
+{
+    public partial class Form1 : Form
+    {
+        private List<String> ServerRoots { get; set; } 
+        public Form1()
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+            getRootsFromTxt();
+            ServerRoots = getRootsFromTxt();
+            FillListElements(ServerRoots);
+            
+        }
+        private void FillListElements(List<String> serverRoots)
+        {
+            foreach (var item in serverRoots)
+            {
+                this.ddlRoot.AddItem(item);
+            }
+        }
+        private List<String> getRootsFromTxt()
+        {
+            List<String> returnList = new List<string>();
+
+            var lines = File.ReadLines(@"C:\Program Files (x86)\HP Inc\TpsFileEditorSetup\roots.txt");
+            foreach (var line in lines)
+                returnList.Add(line);
+            
+            return returnList;
+        }
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            if(proveriPolja() && proveriInstalaciju())
+            {
+                popuniLastLockerStatusStateFile();
+                overridePaketomatId();
+                overrideNumberOfLockersOnPacketMachine();
+                overrideServerRoot();
+                MessageBox.Show("Izmena je uspesno izvrsena!");
+            }
+        }
+
+        private bool proveriPolja()
+        {
+            if(tbPakId.Text == "" || tbBrVrata.Text == "" || ddlRoot.selectedIndex==-1)
+            {
+                MessageBox.Show("Popuni sva polja!");
+                return false;
+            }
+            return true;
+        }
+        
+        private bool proveriInstalaciju()
+        {
+            string putanja = @"C:\Program Files (x86)\HP Inc\TpsApp\TpServisProject.exe";
+            if (!File.Exists(putanja))
+            {
+                MessageBox.Show("Aplikacija nije instalirana, instaliraj aplikaciju!");
+                return false;
+            }
+            return true;
+        }
+
+        private void popuniLastLockerStatusStateFile()
+        {
+            string xmlFile = @"C:\Program Files (x86)\HP Inc\TpsApp\PackageMachineControl\LastLockersStatusState.xml";
+            System.Xml.XmlDocument xmlDoc = new System.Xml.XmlDocument();
+            xmlDoc.Load(xmlFile);
+
+            xmlDoc.SelectSingleNode("root/BreakdownStatuses").InnerText = ispisiSlovo("Z,");
+            xmlDoc.SelectSingleNode("root/BreakdownAdditionalStatuses").InnerText = ispisiSlovo("N,");
+
+            xmlDoc.Save(xmlFile);
+
+        }
+        private string ispisiSlovo(string slovo)
+        {
+            var brojVrata = int.Parse(tbBrVrata.Text);
+            string text = "";
+            for (int i = 0; i < brojVrata; i++)
+            {
+                text += slovo;
+            }
+            return text;
+        }
+
+        private void overridePaketomatId()
+        {
+            XDocument doc = XDocument.Load(@"C:\Program Files (x86)\HP Inc\TpsApp\TpServisProject.exe.config");
+            var list = from appNode in doc.Descendants("appSettings").Elements()
+                       where appNode.Attribute("key").Value == "PaketomatID"
+                       select appNode;
+            var e = list.FirstOrDefault();
+
+            e.Attribute("value").SetValue(tbPakId.Text);
+
+            doc.Save(@"C:\Program Files (x86)\HP Inc\TpsApp\TpServisProject.exe.config");
+        }
+        private void overrideNumberOfLockersOnPacketMachine()
+        {
+            XDocument doc = XDocument.Load(@"C:\Program Files (x86)\HP Inc\TpsApp\TpServisProject.exe.config");
+            var list = from appNode in doc.Descendants("appSettings").Elements()
+                       where appNode.Attribute("key").Value == "NumberOfLockersOnPacketMachine"
+                       select appNode;
+            var e = list.FirstOrDefault();
+
+            e.Attribute("value").SetValue(tbBrVrata.Text);
+
+            doc.Save(@"C:\Program Files (x86)\HP Inc\TpsApp\TpServisProject.exe.config");
+        }
+        private void overrideServerRoot()
+        {
+            XDocument doc = XDocument.Load(@"C:\Program Files (x86)\HP Inc\TpsApp\TpServisProject.exe.config");
+            var list = from appNode in doc.Descendants("appSettings").Elements()
+                       where appNode.Attribute("key").Value == "baseUrl"
+                       select appNode;
+            var e = list.FirstOrDefault();
+
+            e.Attribute("value").SetValue(ddlRoot.selectedValue);
+
+            doc.Save(@"C:\Program Files (x86)\HP Inc\TpsApp\TpServisProject.exe.config");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
